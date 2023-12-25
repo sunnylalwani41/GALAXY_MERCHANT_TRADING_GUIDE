ebd867b8f6b17eccd04ff77ce22653531d1503b0
sunnylalwani41 <sunnylalwani41@gmail.com>
2023-12-25 16:16:56 +0530

    index.jsp file created and server port changed

---
+++ GalaxyMerchantGuidApplication.java
@@ -0,0 +1,13 @@
+package io.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyMerchantGuidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyMerchantGuidApplication.class, args);
+	}
+
+}
---
+++ controller/EndPointMapping.java
@@ -0,0 +1,8 @@
+package io.coffee.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EndPointMapping {
+
+}
